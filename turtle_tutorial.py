8d5478d2537d934a0257a680df2d5e46d9b06f83
Priyanshu Sharma <ps4798214@gmail.com>
2023-11-29 22:44:50 +0530

    add GuiPoint and GuiRectangle

---
+++ turtle_tutorial.py
@@ -0,0 +1,17 @@
+import turtle
+
+myTrutle = turtle.Turtle()
+
+myTrutle.penup()
+myTrutle.goto(50, 75)
+
+myTrutle.pendown()
+myTrutle.forward(100)
+myTrutle.left(90)
+myTrutle.forward(200)
+myTrutle.left(90)
+myTrutle.forward(100)
+myTrutle.left(90)
+myTrutle.forward(200)
+
+turtle.done()
